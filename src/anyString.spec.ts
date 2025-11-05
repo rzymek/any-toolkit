c12bd54c7c30d3609e562a44f0f4f5b814612a8f
rzymek <krzysztof.rzymkowski@gmail.com>
2025-11-05 21:05:15 +0100

    anyString refactor

--- src/anyString.spec.ts
+++ src/anyString.spec.ts
@@ -1,16 +1,32 @@
 import { describe, expect, it } from 'vitest';
-import { anyIdentifier, anyString, anyStringOfLength } from './anyString';
+import { anyString, anyIdentifier, anyPrintableString } from './anyString';
 import { anyInt } from './anyInt';
 
 describe('anyString', () => {
-  it('regex', () => {
+  it('anyString(/RegExp/)', () => {
     expect(anyString(/<([a-z]\w{0,20})>foo<\1>/)).toEqual(
       expect.stringMatching(/<([a-z]\w{0,20})>foo<\1>/),
     );
     expect(anyString(/.+/).length).toBeGreaterThan(0);
   });
 
-  it('anyIdentifier', () => {
+  it('anyString(length)', () => {
+    const length = anyInt(16, 256);
+    expect(anyString(length)).toHaveLength(length);
+  });
+
+  it('anyPrintableString(length)', () => {
+    const length = anyInt(16, 256);
+    const str = anyPrintableString(length);
+    expect(str).toHaveLength(length);
+    for (let i = 0; i < str.length; i++) {
+      const char: number = str.charCodeAt(i);
+      expect(char).toBeGreaterThanOrEqual(32);
+      expect(char).toBeLessThanOrEqual(126);
+    }
+  });
+
+  it('anyIdentifier()', () => {
     expect(anyIdentifier().length).toBeGreaterThan(0);
     expect(anyIdentifier()).toEqual(
       expect.stringMatching(/^[a-zA-Z_][a-zA-Z0-9_]*$/),
@@ -24,10 +40,5 @@ describe('anyString', () => {
     expect(identifier).toEqual(
       expect.stringMatching(/^[a-zA-Z_][a-zA-Z0-9_]*$/),
     );
-  });
-
-  it('anyStringOfLength', () => {
-    const length = anyInt(16, 256);
-    expect(anyStringOfLength(length)).toHaveLength(length);
   });
 });
--- src/anyString.ts
+++ src/anyString.ts
@@ -1,23 +1,27 @@
 import RandExp from 'randexp';
 
-export function anyString(matching: RegExp) {
-  return anyStringMatchingRegExp(matching);
+export function anyString(length: number): string;
+export function anyString(matching: RegExp): string;
+export function anyString(arg: RegExp | number) {
+  return typeof arg === 'number'
+    ? anyStringOfLength(arg)
+    : anyStringMatchingRegExp(arg);
 }
 
-export function anyStringOfLength(length: number) {
-  return anyStringMatchingRegExp(new RegExp(`.{${length}}`));
-}
-
-function anyStringMatchingRegExp(matching: RegExp) {
-  return new RandExp(matching).gen();
-}
-
-export function anyPrintableString() {
-  return anyStringMatchingRegExp(/[ -~]+/);
+export function anyPrintableString(length = 16) {
+  return anyStringMatchingRegExp(new RegExp(`[ -~]{${length}}`));
 }
 
 export function anyIdentifier(length = 16) {
   return anyStringMatchingRegExp(
     new RegExp(`[a-zA-Z_][a-zA-Z0-9_]{${length - 1}}`),
   );
+}
+
+function anyStringOfLength(length: number) {
+  return anyStringMatchingRegExp(new RegExp(`.{${length}}`));
+}
+
+function anyStringMatchingRegExp(matching: RegExp) {
+  return new RandExp(matching).gen();
 }
