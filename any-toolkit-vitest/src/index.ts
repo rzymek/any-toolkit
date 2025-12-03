8ec738b32b7f31b156d80641dc8ba10ea1789225
rzymek <krzysztof.rzymkowski@gmail.com>
2025-12-03 11:01:30 +0100

    minimization safety

--- any-toolkit-vitest/src/index.ts
+++ any-toolkit-vitest/src/index.ts
@@ -1,16 +1,16 @@
 import * as anyToolkit from "any-toolkit";
 import { showActualValueOnError } from "./internal/showActualValueOnError.js";
 
-function wrap<T extends ((...args: any[]) => K) | (()=>K), K>(fn: T):T {
-  return ((...a: Parameters<T>) => showActualValueOnError<K>(fn.name, fn(...a))) as any;
+function wrap<T extends ((...args: any[]) => K) | (() => K), K>(fn: T, name?: string): T {
+  return ((...a: Parameters<T>) => showActualValueOnError<K>(name ?? fn.name, fn(...a))) as any;
 }
 
-export const anyBoolean = wrap(anyToolkit.anyBoolean);
-export const anyDate = wrap(anyToolkit.anyDate);
-export const anyError = wrap(anyToolkit.anyError);
-export const anyFloat = wrap(anyToolkit.anyFloat);
-export const anyInt = wrap(anyToolkit.anyInt);
-export const anyOf = wrap(anyToolkit.anyOf);
-export const anyString = wrap(anyToolkit.anyString);
-export const anyPrintableString = wrap(anyToolkit.anyPrintableString);
-export const anyIdentifier = wrap(anyToolkit.anyIdentifier);
+export const anyBoolean = wrap(anyToolkit.anyBoolean, "anyBoolean");
+export const anyDate = wrap(anyToolkit.anyDate, "anyDate");
+export const anyError = wrap(anyToolkit.anyError, "anyError");
+export const anyFloat = wrap(anyToolkit.anyFloat, "anyFloat");
+export const anyInt = wrap(anyToolkit.anyInt, "anyInt");
+export const anyOf = wrap(anyToolkit.anyOf, "anyOf");
+export const anyString = wrap(anyToolkit.anyString, "anyString");
+export const anyPrintableString = wrap(anyToolkit.anyPrintableString, "anyPrintableString");
+export const anyIdentifier = wrap(anyToolkit.anyIdentifier, "anyIdentifier");
